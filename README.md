4522210baa75b8587300b8c7228a1e066733a9ad
Nacho Millan <ignacio.millan2.contractor@bbva.com>
2018-06-05 10:10:20 +0200

    Added config via environment variables

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ Are you looking for some system that alerts when your today's spending on AWS ex
 CloudWatch doesn't allow you to track costs in the same day, just past days or monthly predictions.
 
 The exporter is a Python server that connects to AWS Cost Explorer with a customizable period, and exposes last responses as Prometheus metrics.
+
+## Configuration
+Configuration is made through environment variables:
+
+| Environment variable        | Description           | Default  |
+| ------------- |:-------------:| -----:|
+| QUERY_PERIOD      | Period to update metrics, querying AWS Cost Explorer API (0.01$ per request) | 1800 |
+| METRIC_TODAY_DAILY_COSTS      | Enable aws_today_daily_costs metric      |   Not set |
+| METRIC_YESTERDAY_DAILY_COSTS | Enable aws_yesterday_daily_costs metric      |   Not set |
+| METRIC_TODAY_DAILY_USAGE | Enable aws_today_daily_usage metric      |   Not set |
+| METRIC_TODAY_DAILY_USAGE_NORM | Enable aws_today_daily_usage_norm metric      |   Not set |
--- app.py
+++ app.py
@@ -13,10 +13,14 @@ app = Flask(__name__)
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
 client = boto3.client('ce')
 
-g_cost = Gauge('aws_today_daily_costs', 'Today daily costs from AWS')
-g_yesterday = Gauge('aws_yesterday_daily_costs', 'Yesterday daily costs from AWS')
-g_usage = Gauge('aws_today_daily_usage', 'Today daily usage from AWS')
-g_usage_norm = Gauge('aws_today_daily_usage_norm', 'Today daily usage normalized from AWS')
+if os.environ.get('METRIC_TODAY_DAILY_COSTS') is not None:
+    g_cost = Gauge('aws_today_daily_costs', 'Today daily costs from AWS')
+if os.environ.get('METRIC_YESTERDAY_DAILY_COSTS') is not None:
+    g_yesterday = Gauge('aws_yesterday_daily_costs', 'Yesterday daily costs from AWS')
+if os.environ.get('METRIC_TODAY_DAILY_USAGE') is not None:
+    g_usage = Gauge('aws_today_daily_usage', 'Today daily usage from AWS')
+if os.environ.get('METRIC_TODAY_DAILY_USAGE_NORM') is not None:
+    g_usage_norm = Gauge('aws_today_daily_usage_norm', 'Today daily usage normalized from AWS')
 
 scheduler = BackgroundScheduler()
 
@@ -25,54 +29,63 @@ def aws_query():
     now = datetime.now()
     yesterday = datetime.today() - timedelta(days=1)
     two_days_ago = datetime.today() - timedelta(days=2)
-    r = client.get_cost_and_usage(
-        TimePeriod={
-            'Start': yesterday.strftime("%Y-%m-%d"),
-            'End':  now.strftime("%Y-%m-%d")
-        },
-        Granularity="DAILY",
-        Metrics=["BlendedCost"]
-    )
-    cost = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
-    print("Updated AWS Daily costs: %s" %(cost))
-    g_cost.set(float(cost))
+    if os.environ.get('METRIC_TODAY_DAILY_COSTS') is not None:
 
-    r = client.get_cost_and_usage(
-        TimePeriod={
-            'Start': two_days_ago.strftime("%Y-%m-%d"),
-            'End':  yesterday.strftime("%Y-%m-%d")
-        },
-        Granularity="DAILY",
-        Metrics=["BlendedCost"]
-    )
-    cost_yesterday = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
-    print("Yesterday's AWS Daily costs: %s" %(cost_yesterday))
-    g_yesterday.set(float(cost_yesterday))
+        r = client.get_cost_and_usage(
+            TimePeriod={
+                'Start': yesterday.strftime("%Y-%m-%d"),
+                'End':  now.strftime("%Y-%m-%d")
+            },
+            Granularity="DAILY",
+            Metrics=["BlendedCost"]
+        )
+        cost = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
+        print("Updated AWS Daily costs: %s" %(cost))
+        g_cost.set(float(cost))
+
+    if os.environ.get('METRIC_YESTERDAY_DAILY_COSTS') is not None:
+        r = client.get_cost_and_usage(
+            TimePeriod={
+                'Start': two_days_ago.strftime("%Y-%m-%d"),
+                'End':  yesterday.strftime("%Y-%m-%d")
+            },
+            Granularity="DAILY",
+            Metrics=["BlendedCost"]
+        )
+        cost_yesterday = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
+        print("Yesterday's AWS Daily costs: %s" %(cost_yesterday))
+        g_yesterday.set(float(cost_yesterday))
 
 
-    r = client.get_cost_and_usage(
-        TimePeriod={
-            'Start': yesterday.strftime("%Y-%m-%d"),
-            'End':  now.strftime("%Y-%m-%d")
-        },
-        Granularity="DAILY",
-        Metrics=["UsageQuantity"]
-    )
-    usage = r["ResultsByTime"][0]["Total"]["UsageQuantity"]["Amount"]
-    print("Updated AWS Daily Usage: %s" %(usage))
-    g_usage.set(float(usage))
+    if os.environ.get('METRIC_TODAY_DAILY_USAGE') is not None:
+        r = client.get_cost_and_usage(
+            TimePeriod={
+                'Start': yesterday.strftime("%Y-%m-%d"),
+                'End':  now.strftime("%Y-%m-%d")
+            },
+            Granularity="DAILY",
+            Metrics=["UsageQuantity"]
+        )
+        usage = r["ResultsByTime"][0]["Total"]["UsageQuantity"]["Amount"]
+        print("Updated AWS Daily Usage: %s" %(usage))
+        g_usage.set(float(usage))
 
-    r = client.get_cost_and_usage(
-        TimePeriod={
-            'Start': yesterday.strftime("%Y-%m-%d"),
-            'End':  now.strftime("%Y-%m-%d")
-        },
-        Granularity="DAILY",
-        Metrics=["NormalizedUsageAmount"]
-    )
-    usage_norm = r["ResultsByTime"][0]["Total"]["NormalizedUsageAmount"]["Amount"]
-    print("Updated AWS Daily Usage Norm: %s" %(usage_norm))
-    g_usage_norm.set(float(usage_norm))
+    if os.environ.get('METRIC_TODAY_DAILY_USAGE_NORM') is not None:
+
+        r = client.get_cost_and_usage(
+            TimePeriod={
+                'Start': yesterday.strftime("%Y-%m-%d"),
+                'End':  now.strftime("%Y-%m-%d")
+            },
+            Granularity="DAILY",
+            Metrics=["NormalizedUsageAmount"]
+        )
+        usage_norm = r["ResultsByTime"][0]["Total"]["NormalizedUsageAmount"]["Amount"]
+        print("Updated AWS Daily Usage Norm: %s" %(usage_norm))
+        g_usage_norm.set(float(usage_norm))
+
+    print("Finished calculating costs")
+
     return 0
 
 
